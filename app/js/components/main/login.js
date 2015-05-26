d47ef5f369a40957209af061ab4de48fc946079a
Barton A Petersen <bart.petersen@gmail.com>
2015-05-26 02:51:59 -0600

    Adding real login page.  Still needs glyph fonts.

--- app/js/components/main/login.js
+++ app/js/components/main/login.js
@@ -2,7 +2,14 @@
 
 import React            from 'react';
 import BaseComponent    from "../base_component";
-import {Styles}         from "material-ui";
+import 
+	{
+		Styles, 
+		Paper, 
+		TextField,
+		FlatButton,
+		FloatingActionButton
+	}                   from "material-ui";
 
 const Colors = Styles.Colors; 
 const Spacing = Styles.Spacing; 
@@ -10,20 +17,71 @@ const Typography = Styles.Typography;
 const ThemeManager = new Styles.ThemeManager().getCurrentTheme();
 
 class Login extends BaseComponent{
-  render(){
-  	let divStyle = {
-  		root: {
-  			paddingTop: Spacing.desktopKeylineIncrement,
-  			fontFamily: 'Roboto'
-  		},
-  		text: {
-  			backgroundColor: Colors.yellow500,
-  		}
-  	};
-    return <div style={divStyle.root}> 
-      <h2 style={divStyle.text}>Login</h2>
-    </div>;
-  }
+	constructor() {
+		super();
+		this.state = {
+			passwordValidationText: "",
+			usernameValidationText: "",
+			error: false
+		};
+	}	
+
+	render(){
+		let divStyle = {
+			root: {
+				width: 345,
+				margin: 'auto',
+				paddingTop: Spacing.desktopKeylineIncrement,
+			},
+			container: {
+				textAlign: 'center',
+				padding: 23
+			},
+			loginText: {
+				fontFamily: 'Roboto',
+				textAlign: 'left',
+				fontWeight: 400,
+			},
+			oauthButton: {
+				margin: 10
+			},
+		};
+
+		return <div style={divStyle.root}> 
+			<Paper>
+			<form onSubmit={this.handleSubmit}>
+				<div style={divStyle.container}>
+					<h2 style={divStyle.loginText}>Login</h2>
+					<TextField 
+						ref="email"
+						hintText='joe@example.com'
+						errorText={this.state.usernameValidationText}
+						floatingLabelText='username'
+						onBlur={this.validateUsernameInput} />
+					<TextField 
+						ref="pass"
+						type='password'
+						hintText='password1'
+						errorText={this.state.passwordValidationText}
+						floatingLabelText='password'
+						onBlur={this.validatePasswordInput} />
+					<FlatButton style={divStyle.submitButton} className='sign-in-button'
+						primary={true} type="submit" label="Login" />	
+				</div>
+			</form>
+	            <div style={divStyle.container} >
+					<FloatingActionButton style={divStyle.oauthButton} iconClassName='icon-google' mini={true} secondary={true} onClick={()=> this._oAuthLogin('google')} />
+					<FloatingActionButton style={divStyle.oauthButton} iconClassName='icon-facebook' mini={true} secondary={true} onClick={()=> this._oAuthLogin('facebook')} />
+					<FloatingActionButton style={divStyle.oauthButton} iconClassName='icon-twitter' mini={true} secondary={true} onClick={()=> this._oAuthLogin('twitter')} />
+	            </div>
+			</Paper>
+		</div>;
+	}
+
+	handleSubmit(e){
+		e.preventDefault();
+		console.log("submitting!");
+	}
 }
 
 module.exports = Login;
