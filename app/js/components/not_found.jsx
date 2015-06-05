f69cd3f8145e3c58e45450f451b42d9cbac5c7b4
Barton A Petersen <bart.petersen@gmail.com>
2015-06-05 01:17:22 -0600

    Styling page not found so it will show up underneath the banner instead of behind it.

--- app/js/components/not_found.jsx
+++ app/js/components/not_found.jsx
@@ -1,9 +1,22 @@
 "use strict";
 
-import React from 'react';
+import React       from 'react';
+import {Styles}    from 'material-ui';
+
+const Spacing = Styles.Spacing; 
 
 export default React.createClass({
   render(){
-    return <h2>Not Found</h2>;
+		let styles = {
+			root: {
+				width: 345,
+				margin: 'auto',
+				paddingTop: Spacing.desktopKeylineIncrement,
+			}
+		};
+    return <div style={styles.root}>
+      <h2>Page Not Found</h2>
+    </div>;
   }
 });
+
